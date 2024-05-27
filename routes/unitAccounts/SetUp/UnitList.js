ce0d9ea3b672593730740dcecd0cc549f0eea948
kshashi183 <shashidhar.k@ahana.co.in>
2024-05-27 14:43:01 +0530

    unit accouts backend updated   27-5-2024

--- routes/unitAccounts/SetUp/UnitList.js
+++ routes/unitAccounts/SetUp/UnitList.js
@@ -1,41 +1,24 @@
 const unitlist = require("express").Router();
 // const cors = require('cors');
 // const { dbco, dbco1, dbgetData, deleteUnitData, updateUnitData } = require("../../../helpers/dbconn")
-const { setupQueryMod } = require("../../../helpers/dbconn")
-var bodyParser = require('body-parser')
+const { setupQueryMod } = require("../../../helpers/dbconn");
+var bodyParser = require("body-parser");
 
-
-
-
-unitlist.get('/getUnitData', (req, res, next) => {
-console.log();
-    try {
-
-        setupQueryMod(`SELECT * FROM magod_setup.magodlaser_units`, (err, data) => {
-            if (err) {
-                console.log("err in query", err);
-            }
-            else {
-             
-                return res.json({ Status: 'Success', Result: data });
-            }
-
-        });
-    } catch (error) {
-        console.log("error", error);
-        next(error);
-    }
+unitlist.get("/getUnitData", (req, res, next) => {
+  console.log();
+  try {
+    setupQueryMod(`SELECT * FROM magod_setup.magodlaser_units`, (err, data) => {
+      if (err) {
+        console.log("err in query", err);
+      } else {
+        console.log("unit");
+        return res.json({ Status: "Success", Result: data });
+      }
+    });
+  } catch (error) {
+    console.log("error", error);
+    next(error);
+  }
 });
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = unitlist;
---
+++ routes/unitAccounts/mailer.js
@@ -0,0 +1,39 @@
+const mailRouter = require("express").Router();
+var createError = require('http-errors');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
+
+const { sendQuotation, sendAttachmails } = require('../../helpers/sendmail');
+
+mailRouter.post('/sendmail', async (req, res, next) => {
+    try {
+        const { customer, qtnDetails, qtnTC } = req.body;
+        sendQuotation(customer, qtnDetails, qtnTC, (err, data) => {
+            if (err) return createError(500, err);
+            else res.send({ status: 'success', data });
+        });
+    } catch (error) {
+        next(error)
+    }
+});
+
+mailRouter.post('/sendDirectMail', upload.single("file"), async (req, res, next) => {
+    console.log("mail sending");
+    try {
+        console.log("req  body mail", req.body);
+        const { toAddress, ccAddress, subjectLine, mailBody,fromAddress } = req.body;
+        sendAttachmails(fromAddress,toAddress, ccAddress, subjectLine, mailBody, {
+            filename: req.file.originalname,
+            path: req.file.path
+       
+        }, (data) => {
+          
+            res.send({status: 'success'});
+            console.log(data);
+        })
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = mailRouter;
